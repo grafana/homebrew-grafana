35e48d706bfabb4e3a494f57d848e94eb5a6ac4f
ctdk <jbingham@gmail.com>
2016-07-07 10:48:56 -0700

    allow brew test to run correctly on Mavericks & ruby 1.8.7 (????)

--- grafana.rb
+++ grafana.rb
@@ -90,7 +90,9 @@ class Grafana < Formula
 
   test do
     system bin/"grafana-server", "-v"
-    Dir.mktmpdir do |tdir|
+    RUBY_VERSION =~ /^(\d+\.\d+)/
+    v = $1.to_f
+    Dir.mktmpdir("grafana", HOMEBREW_TEMP) do |tdir|
       Dir.chdir(pkgshare)
       logdir = File.join(tdir, "log")
       datadir = File.join(tdir, "data")
@@ -98,8 +100,18 @@ class Grafana < Formula
       [logdir, datadir, plugdir].each do |d|
         Dir.mkdir(d)
       end
-      r, w = IO.pipe
-      pid = spawn(bin/"grafana-server", "cfg:default.paths.logs=#{logdir}", "cfg:default.paths.data=#{datadir}", "cfg:default.paths.plugins=#{plugdir}", :err => w)
+
+      r, w, pid = nil
+      if v >= 1.9
+        r, w = IO.pipe
+        pid = spawn(bin/"grafana-server", "cfg:default.paths.logs=#{logdir}", "cfg:default.paths.data=#{datadir}", "cfg:default.paths.plugins=#{plugdir}", :err => w)
+      else
+        require "pty"
+        res = PTY.spawn(bin/"grafana-server", "cfg:default.paths.logs=#{logdir}", "cfg:default.paths.data=#{datadir}", "cfg:default.paths.plugins=#{plugdir}")
+        r = res[0]
+        w = res[1]
+        pid = res[2]
+      end
       sleep 3 # Let it have a chance to actually start up
       Process.kill("TERM", pid)
       w.close
