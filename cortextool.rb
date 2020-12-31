6395748e45392020075f13cdc893a8a0ad636845
cortex-tools-ci <support@grafana.com>
2020-12-31 15:14:59 +0000

    Brew formula update for cortextool version v0.7.0

--- cortextool.rb
+++ cortextool.rb
@@ -2,16 +2,16 @@
 class Cortextool < Formula
   desc "Tools for interacting with Cortex"
   homepage "https://grafana.com"
-  version "0.6.1"
+  version "0.7.0"
   bottle :unneeded
 
   if OS.mac?
-    url "https://github.com/grafana/cortex-tools/releases/download/v0.6.1/cortextool_0.6.1_darwin_amd64.tar.gz"
-    sha256 "23641fef98918a524a92957e0e326075fbf1e42f1962011ca3aab955c7e50c07"
+    url "https://github.com/grafana/cortex-tools/releases/download/v0.7.0/cortextool_0.7.0_darwin_amd64.tar.gz"
+    sha256 "9486a568aa5733b1cd26b73136c9f0904327929bd2254692d8225b0b0b7040cb"
   elsif OS.linux?
     if Hardware::CPU.intel?
-      url "https://github.com/grafana/cortex-tools/releases/download/v0.6.1/cortextool_0.6.1_linux_amd64.tar.gz"
-      sha256 "be36225776a98b7d13c254551625163ec772dbc2982598ad332bada9281f0245"
+      url "https://github.com/grafana/cortex-tools/releases/download/v0.7.0/cortextool_0.7.0_linux_amd64.tar.gz"
+      sha256 "099f6be8117a9c20f9aff443c05a30c4a0ac9b9ad2f68e8e57bde4b6d3a01039"
     end
   end
 
