a47b00cdec72087be8ff1163750994d891214d03
Grot (@grafanabot) <43478413+grafanabot@users.noreply.github.com>
2022-10-03 13:55:41 +0200

    grafana-agent 0.28.0 (#37)

--- grafana-agent.rb
+++ grafana-agent.rb
@@ -1,8 +1,8 @@
 class GrafanaAgent < Formula
   desc "Exporter for Prometheus Metrics, Loki Logs, and Tempo Traces"
   homepage "https://grafana.com/docs/agent/"
-  url "https://github.com/grafana/agent/archive/refs/tags/v0.27.0.tar.gz"
-  sha256 "5c51550406f0df79511f30892a8f95406d83cb492639db00748d825caafcfd45"
+  url "https://github.com/grafana/agent/archive/refs/tags/v0.28.0.tar.gz"
+  sha256 "094b5bb33931649a5db0ee233c0055eb464f10d5c7db7004607d9afb16c7f449"
   license "Apache-2.0"
 
   bottle do
