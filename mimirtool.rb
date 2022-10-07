9ec51ec0e26aeb3f8f33393e3d4aec48c7510fad
Joel Vasallo <joel.vasallo@aspendental.com>
2022-10-07 01:17:56 -0500

    mimirtool 2.3.1
    fixed issues with brew audit

--- mimirtool.rb
+++ mimirtool.rb
@@ -1,5 +1,5 @@
 class Mimirtool < Formula
-  desc "Mimirtool is a command-line tool that operators and tenants can use to execute a number of common tasks that involve Grafana Mimir or Grafana Cloud Metrics."
+  desc "CLI tool for operating/managing Grafana Mimir or GrafanaCloud Metrics"
   homepage "https://grafana.com/docs/mimir/latest/operators-guide/tools/mimirtool/"
   url "https://github.com/grafana/mimir/releases/"
   version "2.3.1"
@@ -7,7 +7,7 @@ class Mimirtool < Formula
   license "AGPL-3.0-only"
 
   on_macos do
-    if Hardware::CPU.arm?
+    on_arm do
       url "https://github.com/grafana/mimir/releases/download/mimir-2.3.1/mimirtool-darwin-arm64"
       sha256 "e6ec1a30d062e775265e94a685fdad090817a038db8168fea65a3064bd533be5"
 
@@ -15,7 +15,7 @@ class Mimirtool < Formula
         bin.install "mimirtool-darwin-arm64" => "mimirtool"
       end
     end
-    if Hardware::CPU.intel?
+    on_intel do
       url "https://github.com/grafana/mimir/releases/download/mimir-2.3.1/mimirtool-darwin-amd64"
       sha256 "bcaf732700c1ce631a3137a935239cc52d2e5dfcf1b1c581d96bc9951c57d509"
 
@@ -26,7 +26,7 @@ class Mimirtool < Formula
   end
 
   on_linux do
-    if Hardware::CPU.arm?
+    on_arm do
       url "https://github.com/grafana/mimir/releases/download/mimir-2.3.1/mimirtool-linux-arm64"
       sha256 "4c5b09385ed8bb4f141450fe511ba1671b40eb9b543e43de2ca0823e87e1f594"
 
@@ -34,7 +34,7 @@ class Mimirtool < Formula
         bin.install "mimirtool-linux-arm64" => "mimirtool"
       end
     end
-    if Hardware::CPU.intel?
+    on_intel do
       url "https://github.com/grafana/mimir/releases/download/mimir-2.3.1/mimirtool-linux-amd64"
       sha256 "1d1de1948963e05f6f3706613fc2bac235c4b8f0205cfd200b6588790bfe1511"
 
@@ -46,6 +46,6 @@ class Mimirtool < Formula
 
   test do
     assert_match "Mimirtool, version 2.3.1 (branch: release-2.3, revision: 64a71a566)",
-      shell_output("#{bin}/mimirtool version", 0)
+      shell_output("#{bin}/mimirtool version")
   end
 end
