cc98ec4b773512a9fcca00105f49f9097748baca
Grot (@grafanabot) <43478413+grafanabot@users.noreply.github.com>
2024-03-14 16:31:50 +0000

    grafana-agent-flow 0.40.3

--- grafana-agent-flow.rb
+++ grafana-agent-flow.rb
@@ -1,8 +1,8 @@
 class GrafanaAgentFlow < Formula
   desc "Vendor-neutral programmable observability pipelines."
   homepage "https://grafana.com/docs/agent/latest/flow"
-  url "https://github.com/grafana/agent/archive/refs/tags/v0.40.2.tar.gz"
-  sha256 "9ed7f2c0de014277afaa10e1618ff2d75731c229ac83d95986fdd8065838b147"
+  url "https://github.com/grafana/agent/archive/refs/tags/v0.40.3.tar.gz"
+  sha256 "a3fd909f344e061f3afbda1ccbc928baa9bff63dbc172838bcb861719959e7c9"
   license "Apache-2.0"
 
   depends_on "go" => :build
