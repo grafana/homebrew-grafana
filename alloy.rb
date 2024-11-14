734a14c6b8cba3024be21b1aebb005e3737fe0e5
Piotr <17101802+thampiotr@users.noreply.github.com>
2024-11-14 13:51:06 +0000

    Pin Node to 20

--- alloy.rb
+++ alloy.rb
@@ -6,7 +6,7 @@ class Alloy < Formula
     license "Apache-2.0"
   
     depends_on "go@1.22" => :build
-    depends_on "node" => :build
+    depends_on "node@20" => :build
     depends_on "yarn" => :build
 
     on_linux do
