d9ceb7504953a620fe7cf0ab2dfc21efa51d97b6
Alexander Akhmetov <me@alx.cx>
2026-05-20 19:08:29 +0200

    sigil cli 0.4.0 (#169)

--- sigil.rb
+++ sigil.rb
@@ -1,9 +1,9 @@
 class Sigil < Formula
   desc "Hook binary for the Grafana AI Observability (Sigil) agent plugins"
   homepage "https://github.com/grafana/sigil-sdk/tree/main/plugins/sigil"
-  url "https://github.com/grafana/sigil-sdk/archive/refs/tags/plugins/sigil/v0.3.0.tar.gz"
-  version "0.3.0"
-  sha256 "79ca6279186b07446894cba647d78075152b536bb1ce9ac8c6d00fd07aa50942"
+  url "https://github.com/grafana/sigil-sdk/archive/refs/tags/plugins/sigil/v0.4.0.tar.gz"
+  version "0.4.0"
+  sha256 "0ebbaaf0a87a17fc1ad8e8e010c1cb50fd19fe2092e83bac018eddec69a65441"
   license "Apache-2.0"
   head "https://github.com/grafana/sigil-sdk.git", branch: "main"
 
